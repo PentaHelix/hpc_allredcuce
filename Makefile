b500400cf4564ab52a80c009c739772295de663d
Jakob Kuen <jakkuen@gmail.com>
2021-12-23 22:26:59 +0100

    Reworked task1

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
+run1: task1
+	mpirun -np 10 task1
+
 run2: task2
 	mpirun -np 7 task2
 
-run1: task1
-	mpirun -np 10 task1
 
 task1: task1.c
 	mpicc task1.c -lm -o task1
--- task1.c
+++ task1.c
@@ -6,41 +6,57 @@
 #include <math.h>
 
 #define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define max(X, Y) (((X) > (Y)) ? (X) : (Y))
 
 int blocksize = 1;
 
-int depth(int val) {
-  // __builtin_ctz: index of the least significant set bit
+int get_layer(int val) {
   return __builtin_ctz(~val);
 }
 
-int parent (int node) {
-  int D = depth(node);
+int get_parent (int node) {
+  int D = get_layer(node);
   int p = node >> (D+2) << (D+2);
   return p | (1 << (D+1)) - 1;
 } 
 
-void nodeinfo(int node, int comm_size, int* ancestor, int* left, int* right, int* layer) {
-  *layer = depth(node);
-  *ancestor = parent(node);
-  int treeheight = floor(log2(comm_size));
-  while (*ancestor >= comm_size && depth(*ancestor) <= treeheight) *ancestor = parent(*ancestor);
-  if (*ancestor >= comm_size) *ancestor = -1;
-  *left =  *layer == 0 ? -1 : node - (1 << *layer-1);
-  *right = *layer == 0 ? -1 : node + (1 << *layer-1);
-
-  int d = *layer-2;
-  while (*right >= comm_size) {
-    if (d == -1) {
-      *right = -1;
-      break;
-    }
-    *right = *right - (1 << d);
-    d--;
-  }
+int do_skips (int rank, int overshoot) {
+  int skips = 0;
+  while (rank + skips >= overshoot + skips*2) skips++;
+  return rank + skips;
 }
 
-int _Reduce (const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
+int undo_skips (int node, int overshoot) {
+  int skipped = max(0, (node - overshoot + 1)/2);
+  return node - skipped;
+}
+
+void nodeinfo(int rank, int comm_size, int* node, int* parent, int* left, int* right, int* layer, int* treeheight) {
+  *treeheight = floor(log2(comm_size));
+  // no. of processes - size of n-1 tree
+  int overshoot = (comm_size - (pow(2,*treeheight) - 1)) * 2;
+
+  // skip any virtual nodes
+  *node = do_skips(rank, overshoot);
+  *layer = get_layer(*node);
+
+  *parent = get_parent(*node);
+  *left =  *layer == 0 ? -1 : *node - (1 << *layer-1);
+  *right = *layer == 0 ? -1 : *node + (1 << *layer-1);
+
+  if (*left % 2 == 0 && *left >= overshoot) *left = -1;
+  if (*right % 2 == 0 && *right >= overshoot) *right = -1;
+
+  *parent = undo_skips(*parent, overshoot);
+  *left = undo_skips(*left, overshoot);
+  *right = undo_skips(*right, overshoot);
+
+  if (*parent >= comm_size) *parent = -1;
+
+  // printf("rank %d, node %d, parent %d, left %d, right %d, layer %d\n", rank, *node, *parent, *left, *right, *layer);
+}
+
+void _Reduce (const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
   // --- SETUP ---
   int sentblocks = 0;
   int recvdblocks = 0;
@@ -48,12 +64,11 @@ int _Reduce (const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatyp
 
   int comm_size;
   MPI_Comm_size(comm, &comm_size);
-  int treeheight = floor(log2(comm_size));
 
-  int node;
-  MPI_Comm_rank(comm, &node);
-  int ancestor, left, right, layer;
-  nodeinfo(node, comm_size, &ancestor, &left, &right, &layer);
+  int rank;
+  MPI_Comm_rank(comm, &rank);
+  int node, parent, left, right, layer, treeheight;
+  nodeinfo(rank, comm_size, &node, &parent, &left, &right, &layer, &treeheight);
 
   int typesize;
   MPI_Type_size(datatype, &typesize);
@@ -63,25 +78,28 @@ int _Reduce (const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatyp
   int round = -1;
   int rounds = 2*(treeheight + blockcount);
 
+
   while (++round < rounds) {
     // --- SENDING ---
     // is node on the right side of parent to send up (even round => left child sends, odd round => right child sends)
-    bool matchesEvenOdd = ancestor < node && round % 2 == 1 || ancestor > node && round % 2 == 0;
+    bool matchesEvenOdd = parent < rank && round % 2 == 1 || parent > rank && round % 2 == 0;
     // is there data to be sent
     bool hasBlocksToSend = sentblocks != blockcount && (recvdblocks >= 2 || layer == 0);
-    // in case nodes between this and ancestor are missing, make sure the ancestor is ready to receive
-    bool isAncestorReceiving = ancestor != -1 && round/2 >= depth(ancestor) - 1;
+    bool isLayerSending = round/2 >= layer;
+    bool hasParent = parent != -1;
 
-    bool shouldSend = matchesEvenOdd && hasBlocksToSend && isAncestorReceiving;
+    // if (rank == 7) printf("[%d] %d %d %d %d\n", round, matchesEvenOdd, hasBlocksToSend, isLayerSending, hasParent);
+
+    bool shouldSend = matchesEvenOdd && hasBlocksToSend && isLayerSending && hasParent;
 
     // --- RECEIVING ---
-    int descendent = round % 2 == 0 ? left : right;
+    int child = round % 2 == 0 ? left : right;
     // has node already started recieving
     bool isReceiving = round/2 >= layer - 1;
     // are there unrecieved blocks left for this node
     bool hasUnreceived = recvdblocks/2 != blockcount;
-    // does descendent exist
-    bool hasDescendent = descendent != -1;
+    // does child exist
+    bool hasDescendent = child != -1;
 
     bool shouldReceive = isReceiving && hasUnreceived && hasDescendent;
 
@@ -90,13 +108,17 @@ int _Reduce (const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatyp
 
     // --- MPI CALLS ---
     if (shouldSend && shouldReceive) {
-      MPI_Sendrecv(value + typesize*blocksize*sentblocks,       chunk_to_send, datatype, ancestor, 0,
-                  recvbuf + typesize*blocksize*(recvdblocks/2), chunk_to_recv, datatype, descendent, 0,
+      // printf("[%d] send %d -> %d\n", round, rank, parent);
+      // printf("[%d] recv %d -> %d\n", round, child, rank);
+      MPI_Sendrecv(value + typesize*blocksize*sentblocks,       chunk_to_send, datatype, parent, 0,
+                  recvbuf + typesize*blocksize*(recvdblocks/2), chunk_to_recv, datatype, child, 0,
                   comm, MPI_STATUS_IGNORE);
     } else if (shouldSend) {
-      MPI_Send(value + typesize*blocksize*sentblocks, chunk_to_send, datatype, ancestor, 0, comm);
+      // printf("[%d] send %d -> %d\n", round, rank, parent);
+      MPI_Send(value + typesize*blocksize*sentblocks, chunk_to_send, datatype, parent, 0, comm);
     } else if (shouldReceive) {
-      MPI_Recv(recvbuf + typesize*blocksize*(recvdblocks/2), chunk_to_recv, datatype, descendent, 0, comm, MPI_STATUS_IGNORE);
+      // printf("[%d] recv %d -> %d\n", round, child, rank);
+      MPI_Recv(recvbuf + typesize*blocksize*(recvdblocks/2), chunk_to_recv, datatype, child, 0, comm, MPI_STATUS_IGNORE);
     }
 
     // --- LOCAL REDUCTION --- 
@@ -112,7 +134,7 @@ int _Reduce (const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatyp
 
     }
 
-    // increment recvdblocks even if one of the ancestors does not exists, to keep steps in sync
+    // increment recvdblocks even if one of the childs does not exists, to keep steps in sync
     if (isReceiving && hasUnreceived) recvdblocks++;
     if (shouldSend) {
       sentblocks++;
@@ -120,23 +142,20 @@ int _Reduce (const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatyp
   }
 
   memcpy(recvbuf, value, typesize*count);
-
-  return (1 << treeheight) - 1;
 }
 
-void _Broadcast (void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
+void _Broadcast (void *buffer, int count, MPI_Datatype datatype, MPI_Comm comm) {
   int sentblocks = 0;
   int recvdblocks = 0;
   int blockcount = ceil((double)count/blocksize);
 
   int comm_size;
   MPI_Comm_size(comm, &comm_size);
-  int treeheight = floor(log2(comm_size));
 
-  int node;
-  MPI_Comm_rank(comm, &node);
-  int ancestor, left, right, layer;
-  nodeinfo(node, comm_size, &ancestor, &left, &right, &layer);
+  int rank;
+  MPI_Comm_rank(comm, &rank);
+  int node, parent, left, right, layer, treeheight;
+  nodeinfo(rank, comm_size, &node, &parent, &left, &right, &layer, &treeheight);
 
   int typesize;
   MPI_Type_size(datatype, &typesize);
@@ -146,36 +165,37 @@ void _Broadcast (void *buffer, int count, MPI_Datatype datatype, int root, MPI_C
 
   while (++round < rounds) {
     // --- SENDING ---
-    int descendent = round % 2 == 0 ? left : right;
-    // does descendent exist
-    bool hasDescendent = descendent != -1;
-    // is there data left to be send
+    int child = round % 2 == 0 ? left : right;
+    bool hasDescendent = child != -1;
     bool hasBlocksLeft = sentblocks/2 != blockcount && (recvdblocks >= 1 || layer == treeheight);
 
     bool shouldSend =  hasDescendent && hasBlocksLeft;
 
     // --- RECEIVING ---
-    // is node on the right side of parent to send up (even round => left child receives, odd round => right child receives)
-    bool matchesEvenOdd = ancestor < node && round % 2 == 1 || ancestor > node && round % 2 == 0;
+    bool matchesEvenOdd = parent < rank && round % 2 == 1 || parent > rank && round % 2 == 0;
     // are there unreceived blocks left for this node
     bool hasUnreceived = recvdblocks != blockcount;
-    // has the ancestor sent a block this round
-    bool hasSendingAncestor = ancestor != -1 && round/2 >= (treeheight-depth(ancestor));
+    // has the parent sent a block this round
+    bool hasParent = parent != -1 && round/2 >= (treeheight-layer-1);
 
-    bool shouldReceive = matchesEvenOdd && hasUnreceived && hasSendingAncestor;
+    bool shouldReceive = matchesEvenOdd && hasUnreceived && hasParent;
 
     int chunk_to_send = min(count - (sentblocks/2)*blocksize, blocksize);
     int chunk_to_recv = min(count - (recvdblocks)*blocksize, blocksize);
 
     // --- MPI CALLS ---
     if (shouldSend && shouldReceive) {
-      MPI_Sendrecv(buffer + typesize*blocksize*(sentblocks/2), chunk_to_send, datatype, descendent, 0,
-                  buffer + typesize*blocksize*recvdblocks,     chunk_to_recv, datatype, ancestor, 0,
+      // printf("[%d] send %d -> %d\n", round, rank, child);
+      // printf("[%d] recv %d -> %d\n", round, parent, rank);
+      MPI_Sendrecv(buffer + typesize*blocksize*(sentblocks/2), chunk_to_send, datatype, child, 0,
+                  buffer + typesize*blocksize*recvdblocks,     chunk_to_recv, datatype, parent, 0,
                   comm, MPI_STATUS_IGNORE);
     } else if (shouldSend) {
-      MPI_Send(buffer + typesize*blocksize*(sentblocks/2), chunk_to_send, datatype, descendent, 0, comm);
+      // printf("[%d] send %d -> %d\n", round, rank, child);
+      MPI_Send(buffer + typesize*blocksize*(sentblocks/2), chunk_to_send, datatype, child, 0, comm);
     } else if (shouldReceive) {
-      MPI_Recv(buffer + typesize*blocksize*recvdblocks, chunk_to_recv, datatype, ancestor, 0, comm, MPI_STATUS_IGNORE);
+      // printf("[%d] recv %d -> %d\n", round, parent, rank);
+      MPI_Recv(buffer + typesize*blocksize*recvdblocks, chunk_to_recv, datatype, parent, 0, comm, MPI_STATUS_IGNORE);
     }
 
     if (hasBlocksLeft) sentblocks++;
@@ -184,8 +204,8 @@ void _Broadcast (void *buffer, int count, MPI_Datatype datatype, int root, MPI_C
 }
 
 void AllReduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
-  int root = _Reduce(sendbuf, recvbuf, count, datatype, op, comm);
-  _Broadcast(recvbuf, count, datatype, root, comm);
+  _Reduce(sendbuf, recvbuf, count, datatype, op, comm);
+  _Broadcast(recvbuf, count, datatype, comm);
 }
 
 
@@ -247,12 +267,12 @@ int main(int argc, char **argv) {
   // int world_rank;
   // MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  // double out[] = {[0 ... 10000] = 2.124};
-  // double in[10000] = {0.0};
+  // double out[] = {1.0, 1.0, 1.0, 1.0, 1.0};
+  // double in[] = {0.0, 0.0, 0.0, 0.0, 0.0};
 
-  // AllReduce(out, in, 10000, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-  // // MPI_Allreduce(out, in, 10000, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-  // printf("result: (%lf, %lf, %lf, %lf, %lf)\n", in[0], in[240], in[3333], in[7489], in[9999]);
+  // AllReduce(out, in, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  // MPI_Allreduce(out, in, 10000, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  // printf("result: (%lf, %lf, %lf, %lf, %lf)\n", in[0], in[1], in[2], in[3], in[4]);
 
   run_test();
   // Finalize: Any resources allocated for MPI can be freed
